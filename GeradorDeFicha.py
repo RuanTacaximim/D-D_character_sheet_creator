0ebf8bc5fb9a1e3ad91bb9c6c5451ea36ab4ba16
Ruan_Tacaximim <ruan.tacaximim@outlook.com>
2024-02-01 13:05:17 -0300

    deletei o arquivo UI
    decidi colocar tudo no mesmo script, tá bugando menos e com meu conhecimento atual vai bugar menos também !

--- GeradorDeFicha.py
+++ GeradorDeFicha.py
@@ -1,5 +1,7 @@
 from emoji import emojize
 from random2 import randrange
+import tkinter as tk
+import customtkinter
 #variavel para os emogis
 emg_dex = emojize(':person_cartwheeling: :')
 emg_wis = emojize(':scroll: :')
@@ -10,52 +12,54 @@ emg_car = emojize(':new_moon_face: :')
 emg_str = emojize(':person_lifting_weights: :')
 emg_dice = emojize(':game_die: :')
 emg_mcn = emojize(":slot_machine: :")
-#print(emg_car,emg_dex,emg_int,emg_luck,emg_wis,emg_per,emg_str,emg_dice)(debug dos emogis)
-print(emojize( ':crossed_swords:  TACAXI-RPG :crossed_swords:  (D&D simplicado)'))
-NomeJogador = input('Digite seu nome !:')
-personagem = input('OK !, {} agora digite o nome do seu personagem !'.format(NomeJogador))
-print('Agora, você vai precisar usar um dado pra conseguir seu pontos de atributo (D20) OU aceitar os que vierem aqui ! \n \n Você quer rolar  os dados ?',emg_dice, '(1)' ' ou quer atributos aleatórios ?',emg_mcn, '(2)' '\n')
-Answer = int(input('1 ou 2 ? :'))
-#funcção para atributos aleatórios
-def Atributos_auto():
-    dexterity = randrange(4,20)
-    wisdom =  randrange(4,20)
-    Inteligence = randrange(4,20)
-    perception = randrange(4,20)
-    luck = randrange(4,20)
-    strength = randrange (4,20)
-    charisma = randrange (4,20)
-    print("_"*20)
-    print("Personagem:",personagem)
-    print("_"*20)
-    print("Destreza",emg_dex,dexterity,"\n""Sabedoria",emg_wis,wisdom,"\n""Inteligência",emg_int,Inteligence,"\n""Percepsão",emg_per,perception) 
-    print("Sorte",emg_luck,luck,"\n" "Força",emg_str,strength, "\n" "Carisma",emg_car,charisma)
-    print("_"*20)
-    print("Jogador:",NomeJogador)
-    print("_"*20)
-#função para atributos rolados no dado fisico ou discord
-def Atributos_manual():
-    dexterity = int(input(emojize('digite sua destreza !:person_cartwheeling: :')))
-    wisdom = int(input(emojize('digite sua sabedoria !:scroll: :')))
-    Inteligence = int(input(emojize('digite a inteligencia !:brain: :')))
-    perception = int(input(emojize('digite a sua percepção !:eye: :')))
-    luck = int(input(emojize('digite a sua sorte ! :four_leaf_clover: :')))
-    strength = int(input(emojize('digite a sua força ! :person_lifting_weights: :')))
-    charisma = int (input(emojize('digite o quão charmoso tu é :new_moon_face: :')))
-    print("_"*20)
-    print("Personagem:",personagem)
-    print("_"*20)
-    print("Destreza",emg_dex,dexterity,"\n""Sabedoria",emg_wis,wisdom,"\n""Inteligência",emg_int,Inteligence,"\n""Percepsão",emg_per,perception) 
-    print("Sorte",emg_luck,luck,"\n" "Força",emg_str,strength, "\n" "Carisma",emg_car,charisma)
-    print("_"*20)
-    print("Jogador:",NomeJogador)
-    print("_"*20)
 
-#escolhe o algoritmo de contrução da ficha
-if Answer == 1:
-    Atributos_manual()
-if Answer == 2:
-    Atributos_auto()
+def iniciar_programa():
+    print(emojize( ':crossed_swords:  TACAXI-RPG :crossed_swords:  (D&D simplicado)'))
+    NomeJogador = input('Digite seu nome !:')
+    personagem = input('OK !, {} agora digite o nome do seu personagem !'.format(NomeJogador))
+    print('Agora, você vai precisar usar um dado pra conseguir seu pontos de atributo (D20) OU aceitar os que vierem aqui ! \n \n Você quer rolar  os dados ?',emg_dice, '(1)' ' ou quer atributos aleatórios ?',emg_mcn, '(2)' '\n')
+    Answer = int(input('1 ou 2 ? :'))
+    
+    #funcção para atributos aleatórios
+    def Atributos_auto():
+        dexterity = randrange(4,20)
+        wisdom =  randrange(4,20)
+        Inteligence = randrange(4,20)
+        perception = randrange(4,20)
+        luck = randrange(4,20)
+        strength = randrange (4,20)
+        charisma = randrange (4,20)
+        print("_"*20)
+        print("Personagem:",personagem)
+        print("_"*20)
+        print("Destreza",emg_dex,dexterity,"\n""Sabedoria",emg_wis,wisdom,"\n""Inteligência",emg_int,Inteligence,"\n""Percepsão",emg_per,perception) 
+        print("Sorte",emg_luck,luck,"\n" "Força",emg_str,strength, "\n" "Carisma",emg_car,charisma)
+        print("_"*20)
+        print("Jogador:",NomeJogador)
+        print("_"*20)
+    #função para atributos rolados no dado fisico ou discord
+    def Atributos_manual():
+        dexterity = int(input(emojize('digite sua destreza !:person_cartwheeling: :')))
+        wisdom = int(input(emojize('digite sua sabedoria !:scroll: :')))
+        Inteligence = int(input(emojize('digite a inteligencia !:brain: :')))
+        perception = int(input(emojize('digite a sua percepção !:eye: :')))
+        luck = int(input(emojize('digite a sua sorte ! :four_leaf_clover: :')))
+        strength = int(input(emojize('digite a sua força ! :person_lifting_weights: :')))
+        charisma = int (input(emojize('digite o quão charmoso tu é :new_moon_face: :')))
+        print("_"*20)
+        print("Personagem:",personagem)
+        print("_"*20)
+        print("Destreza",emg_dex,dexterity,"\n""Sabedoria",emg_wis,wisdom,"\n""Inteligência",emg_int,Inteligence,"\n""Percepsão",emg_per,perception) 
+        print("Sorte",emg_luck,luck,"\n" "Força",emg_str,strength, "\n" "Carisma",emg_car,charisma)
+        print("_"*20)
+        print("Jogador:",NomeJogador)
+        print("_"*20)
+
+    #escolhe o algoritmo de contrução da ficha
+    if Answer == 1:
+        Atributos_manual()
+    if Answer == 2:
+        Atributos_auto()
 
     
 
@@ -68,6 +72,11 @@ if Answer == 2:
 #3 poder ser legal adicionar ao atributo o valor do bonus de raça
 #4 pode ser legal adicionar a função para rolar dano de ataque e 
 #5 adicionar sub atributos e habilidades nos calculos
-if __name__ == "__main__":
-    print("executado manualmente!")
-    pass
+
+
+root = tk.Tk()
+def on_button_click():
+    iniciar_programa()
+CRIAR = tk.Button( text="gerar ficha", command= on_button_click) 
+CRIAR.pack()
+root.mainloop()
